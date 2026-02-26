d19f3c1e2825cbf3cdaf884e08f092ff63961e3b
RENO <reno@MacBook-Air-de-RENO.local>
2026-02-26 16:42:29 +0100

    init tp

---
+++ src/test/java/fr/epsi_26/bestioles/BestiolesApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.epsi_26.bestioles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestiolesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
